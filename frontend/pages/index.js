0b187199443ba100d38a94cf986b43a6b178e057
Diego Espinoza <diespinozas@gmail.com>
2019-05-22 09:13:51 -0700

    add functions to map the week and renderit on page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,35 @@
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
-);
+const Index = () => {
+  const today = new Date()
 
-export default Index;
+  const findNearMonday = function(date) {
+    const monday = new Date()
+    const difference = date.getDay() === 0 ? 6 : date.getDay() - 1
+    monday.setDate(date.getDate() - difference)
+    return monday
+  }
+
+  const findWeek = function(monday) {
+    let actualDate = new Date()
+    let week = []
+    for (let i = 0; i < 7; i++) {
+      actualDate.setDate(monday.getDate() + i)
+      week[i] = new Date(actualDate)
+    }
+    return week
+  }
+
+  const week = findWeek(findNearMonday(today))
+
+  return (
+    <div>
+      <h1> Appointment reminder</h1>
+      <ul>
+        {week.map(day => (
+          <li>{JSON.stringify(day)}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Index
