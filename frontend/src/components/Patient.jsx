b99b4bde7578c06e2a8720f1ba0f46cecb873d37
Diego Espinoza <diespinozas@gmail.com>
2019-05-25 11:33:51 -0700

    add basic Patient component to create a patient

---
+++ frontend/src/components/Patient.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class CreatePatient extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <form />
+      </div>
+    );
+  }
+}
+
+export default CreatePatient;
